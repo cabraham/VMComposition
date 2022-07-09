619b8fa143a0b9b7eecc91b854a65f8c906b0013
Ciby Abraham <5863494+cabraham@users.noreply.github.com>
2022-07-09 14:01:08 -0400

    Adding ProductInventoryCompositionHandler and related files

---
+++ VMC.Inventory/Composition/ProductInventoryCompositionHandler.cs
@@ -0,0 +1,29 @@
+﻿using VMC.Framework.Composition;
+using VMC.Inventory.Data;
+
+namespace VMC.Inventory.Composition
+{
+    public class ProductInventoryCompositionHandler : IHandleCompositionRequest
+    {
+        private readonly ProductRepository productRepository;
+        private HashSet<string> handlingRequestTypes;
+        public ProductInventoryCompositionHandler(ProductRepository productRepository)
+        {
+            handlingRequestTypes = new HashSet<string>();
+            handlingRequestTypes.Add("INeedToKnowIfProductIsAvailable");
+            handlingRequestTypes.Add("INeedToKnowProductCount");
+            this.productRepository = productRepository;
+        }
+
+        public Task<object> Handle(CompositionRequest request)
+        {
+            var product = productRepository.Get(int.Parse(request.Parameters["productId"]));
+            return Task.FromResult((object)product.Count);
+        }
+
+        public bool IsApplicableHandler(CompositionRequest compositionRequest)
+        {
+            return handlingRequestTypes.Contains(compositionRequest.RequestType);
+        }
+    }
+}
---
+++ VMC.Inventory/Data/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using VMC.Inventory.Models;
+
+namespace VMC.Inventory.Data
+{
+    public class ProductRepository
+    {
+        private Dictionary<int, Product> productInventory;
+        public ProductRepository()
+        {
+            productInventory = new Dictionary<int, Product>();
+            foreach (var i in Enumerable.Range(1,5))
+            {
+                productInventory.Add(i, new Product { Id = i, Name = $"Product {i}" });
+            }
+
+            productInventory[1].Count = 3;
+            productInventory[2].Count = 1;
+            productInventory[3].Count = 6;
+            productInventory[4].Count = 0;
+            productInventory[5].Count = 5;
+        }
+
+        public Product Get(int productId)
+        {
+            return productInventory[productId];
+        }
+    }
+}
