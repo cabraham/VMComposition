45e7c4cebbc0377c71415eb97c2782baf9a4e6a8
Ciby Abraham <5863494+cabraham@users.noreply.github.com>
2022-07-09 21:47:14 -0400

    Adding Sales repo, model, and composition handler

---
+++ VMC.Sales/Composition/ProductPriceCompositionHandler.cs
@@ -0,0 +1,30 @@
+﻿using VMC.Framework.Composition;
+using VMC.Sales.Data;
+
+namespace VMC.Sales.Composition
+{
+    public class ProductPriceCompositionHandler : IHandleCompositionRequest
+    {
+        private readonly ProductRepository productRepository;
+        private HashSet<string> compositionRequestTypes;
+
+        public ProductPriceCompositionHandler(ProductRepository productRepository)
+        {
+            compositionRequestTypes = new HashSet<string>();
+            compositionRequestTypes.Add("INeedToKnowProductPrice");
+            this.productRepository = productRepository;
+        }
+
+        public Task<object> Handle(CompositionRequest request)
+        {
+            var product = productRepository.Get(int.Parse(request.Parameters["productId"]));
+            return Task.FromResult((object)product.Price);
+
+        }
+
+        public bool IsApplicableHandler(CompositionRequest compositionRequest)
+        {
+            return compositionRequestTypes.Contains(compositionRequest.RequestType);
+        }
+    }
+}
---
+++ VMC.Sales/Data/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using VMC.Sales.Models;
+
+namespace VMC.Sales.Data
+{
+    public class ProductRepository
+    {
+        private Dictionary<int, ProductPrice> productPrices;
+
+        public ProductRepository()
+        {
+            productPrices = new Dictionary<int, ProductPrice>();
+            foreach (var i in Enumerable.Range(1, 5))
+            {
+                productPrices.Add(i, new ProductPrice { Id = i, Price = (decimal)3.5 * i });
+            }
+        }
+
+        public ProductPrice Get(int productId)
+        {
+            return productPrices[productId];
+        }
+
+    }
+}
